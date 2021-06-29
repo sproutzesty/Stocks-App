77e8a47213592b29bd9db0a46f61188a6711b94b
Hannah Sinclair <hannahsinclair92@gmail.com>
2021-06-28 21:28:43 -0500

    getting closer to having a working api

--- stocks-app/main/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from stocks.models import Stocks
-
-class StocksSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Stocks
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
